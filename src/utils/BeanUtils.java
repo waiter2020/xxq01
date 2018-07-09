d70a2850a7f6216490dfd6a1735a64eb80532362
waiter2020 <1403976416@qq.com>
2018-07-09 08:41:30 +0800

    修BUG

---
+++ src/utils/BeanUtils.java
@@ -0,0 +1,17 @@
+package utils;
+
+import java.sql.ResultSet;
+
+/**
+ * Created by  waiter on 18-7-9  上午8:29.
+ *
+ * @author waiter
+ */
+public class BeanUtils {
+
+
+    public static Object rsToBean(Class cls , ResultSet set){
+
+        return new Object();
+    }
+}
